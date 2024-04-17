5c3f35d10928de446c1e03d13a5f9aedffe35cf1
Sudo <92053140+cardondev@users.noreply.github.com>
2024-04-17 16:06:26 -0400

    Update set_hst_timestamp.sh

--- Scripts/Bash/set_hst_timestamp.sh
+++ Scripts/Bash/set_hst_timestamp.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+# Loop through all users in the /home directory
 for users in /home/*; do
-
+    # Check if the directory is a valid user home directory
     if [ -d "$users" ]; then
-
+        # Get the username from the directory name
         username=$(basename "$users")
 
-        bashrc_file="$users /.bashrc"
-
-        if [ -f "$bashrc_file" ]; then
-
-            if ! grep -q 'HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S %Z "' "$bashrc_file"; then
-    
-                echo 'export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S %Z "' >> "$bashrc_file"
+        # Check if the user has a .bashrc file
+        bashrc="$users/.bashrc"
+        if [ -f "$bashrc" ]; then
+            # Check if the HISTTIMEFORMAT line already exists in the .bashrc file
+            if ! grep -q 'HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S %Z "' "$bashrc"; then
+                # Add the HISTTIMEFORMAT line to the .bashrc file
+                echo 'export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S %Z "' >> "$bashrc"
                 echo "Added HISTTIMEFORMAT to $username's .bashrc"
             else
                 echo "HISTTIMEFORMAT already exists in $username's .bashrc"
             fi
-            
-            su - "$username" -c "source $bashrc_file"
+
+            # Source the .bashrc file for the user
+            su - "$username" -c "source $bashrc"
             echo "Sourced $username's .bashrc"
         else
-            echo "User $username does not have a .bashrc file"
+            echo "Creating .bashrc file for $username"
+            touch "$bashrc"
+            echo 'export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S %Z "' >> "$bashrc"
+            echo "Added HISTTIMEFORMAT to $username's .bashrc"
+            
+            # Source the .bashrc file for the user
+            su - "$username" -c "source $bashrc"
+            echo "Sourced $username's .bashrc"
         fi
     fi
 done
